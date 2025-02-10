b0f06152caa8bf7fb7eb8c5c7d0fb454399be2c9
Munachi Ernest-Eze <ernest-eze1869@loyolajesuit.org>
2025-02-10 15:12:49 -0500

    Fixed Webpage structure and keywords

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,40 +1,39 @@
 import { NextResponse } from "next/server"
-import nodemailer from "nodemailer"
 
-export async function POST(request: Request) {
-  const { name, email, company, message } = await request.json()
+export async function POST() {
+  // const { name, email, company, message } = await request.json()
 
-  // Create a transporter using SMTP
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // Use TLS
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  })
+  // // Create a transporter using SMTP
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   port: 587,
+  //   secure: false, // Use TLS
+  //   auth: {
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_PASS,
+  //   },
+  // })
 
   // Send mail with defined transport object
   try {
-    await transporter.sendMail({
-      from: '"Ernest Advisory Website" <noreply@ernestadvisory.com>',
-      to: "munachi@createwithjck.com",
-      subject: "New Contact Form Submission",
-      text: `
-        Name: ${name}
-        Email: ${email}
-        Company: ${company}
-        Message: ${message}
-      `,
-      html: `
-        <h1>New Contact Form Submission</h1>
-        <p><strong>Name:</strong> ${name}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Company:</strong> ${company}</p>
-        <p><strong>Message:</strong> ${message}</p>
-      `,
-    })
+    // await transporter.sendMail({
+    //   from: '"Ernest Advisory Website" <noreply@ernestadvisory.com>',
+    //   to: "munachi@createwithjck.com",
+    //   subject: "New Contact Form Submission",
+    //   text: `
+    //     Name: ${name}
+    //     Email: ${email}
+    //     Company: ${company}
+    //     Message: ${message}
+    //   `,
+    //   html: `
+    //     <h1>New Contact Form Submission</h1>
+    //     <p><strong>Name:</strong> ${name}</p>
+    //     <p><strong>Email:</strong> ${email}</p>
+    //     <p><strong>Company:</strong> ${company}</p>
+    //     <p><strong>Message:</strong> ${message}</p>
+    //   `,
+    // })
 
     return NextResponse.json({ message: "Email sent successfully" }, { status: 200 })
   } catch (error) {
